c56bfc5ae19ad64a32495f43372cdbe8e5fd5a3f
Le Quoc Thang <thang.lequoc@petronas.com>
2022-04-13 21:00:31 +0700

    admin receipt

--- AgriShop/Areas/Admin/Controllers/ManageReceiptController.cs
+++ AgriShop/Areas/Admin/Controllers/ManageReceiptController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AnBinhMarket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnBinhMarket.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ManageReceiptController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ManageReceiptController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var hoaDons = _context.HoaDons.Include(x=>x.GioHang).ThenInclude(x=>x.TaiKhoan).Select(h => h);
+            hoaDons = hoaDons.OrderByDescending(s => s.NgayTao);
+            return View(hoaDons.ToList());
+        }
+
+        public IActionResult Details(Guid id)
+        {
+            var hoaDon = _context.HoaDons
+                .Include(x=>x.GioHang).ThenInclude(x=>x.ChiTietGioHangs).ThenInclude(x=>x.SanPham)
+                .Include(x=>x.GioHang).ThenInclude(x=>x.TaiKhoan).FirstOrDefault(x=>x.Id == id);
+
+            if (hoaDon == null)
+            {
+                return NotFound();
+            }
+            return View(hoaDon);
         }
     }
 }
