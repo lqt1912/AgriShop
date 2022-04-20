8402df4112ce4c7b47335c3d9f8a8146a2e8953c
Diep Nguyen Thanh <diep.nguyenthanh@petronas.com>
2022-04-20 19:17:26 +0700

    Change something

--- AgriShop/Areas/Admin/Controllers/ManageReceiptController.cs
+++ AgriShop/Areas/Admin/Controllers/ManageReceiptController.cs
@@ -1,5 +1,6 @@
 ﻿using AnBinhMarket.Data;
 using AnBinhMarket.Data.Entities;
+using AnBinhMarket.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,16 +18,39 @@ namespace AnBinhMarket.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            var hoaDons = _context.HoaDons.Include(x=>x.GioHang).ThenInclude(x=>x.TaiKhoan).Select(h => h);
-            hoaDons = hoaDons.OrderByDescending(s => s.NgayTao);
-            return View(hoaDons.ToList());
+            var hoaDons = _context.HoaDons.Include(x => x.GioHang).ThenInclude(x => x.ChiTietGioHangs).Include(x=>x.GioHang).ThenInclude(x=>x.TaiKhoan).Select(h => h);
+
+            var result = new List<HoaDonViewModel>();
+            foreach (var hoaDon in hoaDons)
+            {
+                decimal thanhTien = 0;
+
+                foreach (var ctgh in hoaDon.GioHang.ChiTietGioHangs)
+                {
+                    thanhTien += ctgh.Gia * ctgh.SoLuong;
+                }
+                result.Add(new HoaDonViewModel()
+                {
+                    ChuY = hoaDon.ChuY,
+                    DiaChi = hoaDon.DiaChi,
+                    MaHoaDon = hoaDon.MaHoaDon,
+                    NgayCapNhat = hoaDon.NgayCapNhat,
+                    NgayTao = hoaDon.NgayTao,
+                    ThanhTien = thanhTien + hoaDon.PhiShip,
+                    TrangThai = hoaDon.TrangThai,
+                     Id = hoaDon.Id, 
+                     HoTen = hoaDon.GioHang.TaiKhoan.HoTen
+                });
+            }
+            result = result.OrderByDescending(x => x.NgayTao).ToList();
+            return View(result);
         }
 
         public IActionResult Details(Guid id)
         {
             var hoaDon = _context.HoaDons
-                .Include(x=>x.GioHang).ThenInclude(x=>x.ChiTietGioHangs).ThenInclude(x=>x.SanPham)
-                .Include(x=>x.GioHang).ThenInclude(x=>x.TaiKhoan).FirstOrDefault(x=>x.Id == id);
+                .Include(x => x.GioHang).ThenInclude(x => x.ChiTietGioHangs).ThenInclude(x => x.SanPham)
+                .Include(x => x.GioHang).ThenInclude(x => x.TaiKhoan).FirstOrDefault(x => x.Id == id);
 
             if (hoaDon == null)
             {
@@ -54,7 +78,7 @@ namespace AnBinhMarket.Areas.Admin.Controllers
         {
             try
             {
-                var _hoaDon = _context.HoaDons.FirstOrDefault(x=>x.Id ==hoaDon.Id);
+                var _hoaDon = _context.HoaDons.FirstOrDefault(x => x.Id == hoaDon.Id);
                 if (_hoaDon != null)
                 {
                     _hoaDon.TrangThai = hoaDon.TrangThai;
